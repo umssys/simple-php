409c9f7dc78626cc0ba9f9a52a7b2d0f217bb15a
jorge.ubaque <jorge.ubaque@A19-DAP-305.aviatur.com.co>
2016-05-20 18:01:12 -0500

    submodulos

---
+++ web/app/core/Kernel.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Kernel {
+
+//    function __construct() {
+//        $this->registrarClases();
+//    }
+//
+//    private function registrarClases() {
+//        spl_autoload_register(function($className) {
+//            if (!file_exists($className . ".php")) {
+//                throw new \Exception("La clase " . $className . " no pudo ser encontrada");
+//            }
+//            include $className . ".php";
+//        });
+//        return true;
+//    }
+
+}
